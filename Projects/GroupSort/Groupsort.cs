8a4e25c5b95ec8d60aac76c920e68e84c5b238e7
BuildTools <unconfigured@null.spigotmc.org>
2022-09-12 20:20:43 -0700

    Make public field private

--- Projects/GroupSort/Groupsort.cs
+++ Projects/GroupSort/Groupsort.cs
@@ -8,7 +8,7 @@ namespace SortingAlgorithms
 {
     public class GroupSort
     {
-        public static double algorithmSwitchoverThreshold = .1;
+        private static double algorithmSwitchoverThreshold = .1;
 
         public class listGrouped
         {
