d5b23e361274f0c0e2e5871387952c81713ef714
BuildTools <unconfigured@null.spigotmc.org>
2026-07-24 19:20:19 -0700

    Add data validation

--- Projects/OperationsKnowledge/Dtos/CreateOperationalSystemRequest.cs
+++ Projects/OperationsKnowledge/Dtos/CreateOperationalSystemRequest.cs
@@ -1,11 +1,20 @@
-﻿namespace OperationsKnowledge.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OperationsKnowledge.Dtos;
 
 public class CreateOperationalSystemRequest
 {
+    [Required(ErrorMessage = "Operational system must have a name.")]
+    [StringLength(150, ErrorMessage = "Length limit of 150 characters.")]
     public string Name { get; set; } = "";
 
+    [Required(ErrorMessage = "Operational system must have a status.")]
+    [StringLength(100, ErrorMessage = "Length limit of 100 characters.")]
     public string Status { get; set; } = "";
 
+    [StringLength(1000, ErrorMessage = "Length limit of 1000 characters.")]
     public string Description { get; set; } = "";
+
+    [StringLength(150, ErrorMessage = "Length limit of 150 characters.")]
     public string Owner { get; set; } = "";
 }
