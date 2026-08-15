a0d35570b9a5cc7f5922dd27137a87ec1e0e2747
BuildTools <unconfigured@null.spigotmc.org>
2026-08-14 17:04:54 -0700

    Add mapper helper

---
+++ Projects/OperationsKnowledge/Mappings/OperationalSystemMapper.cs
@@ -0,0 +1,20 @@
+﻿using OperationsKnowledge.Dtos;
+using OperationsKnowledge.Models;
+
+namespace OperationsKnowledge.Mappings;
+
+public class OperationalSystemMapper
+{
+    public static OperationalSystemResponse ToResponse(OperationalSystem system)
+    {
+        return new OperationalSystemResponse
+        {
+            Id = system.Id,
+            Name = system.Name,
+            Status = system.Status,
+            Description = system.Description,
+            OwnerId = system.OwnerId,
+            OwnerName = system.Owner?.Name
+        };
+    }
+}
